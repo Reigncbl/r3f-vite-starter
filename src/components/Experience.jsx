c004b6a14ab44ef9b6cdcbbc0d24c1dd0ffced61
Reigniel Cabela <148176276+Reigncbl@users.noreply.github.com>
2025-03-11 01:50:27 +0800

    model

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,21 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useThree } from "@react-three/fiber";
+import { useEffect } from "react";
 
 export const Experience = () => {
+  const texture = useTexture("textures/bg.jpg");
+  const { scene } = useThree();
+
+  useEffect(() => {
+    scene.background = texture;
+  }, [scene, texture]);
+
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Avatar position={[0, -2.5, 0]} scale={2} rotation={[0, Math.PI, 0]} />
+      <Environment preset="studio" />
     </>
   );
 };
